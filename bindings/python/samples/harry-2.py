c465844ef25f1272b6e7b4dcf12ca35db3bb9afa
Harrison Steg <35347832+hsteg@users.noreply.github.com>
2024-10-27 22:05:11 -0400

    1

--- bindings/python/samples/harry-2.py
+++ bindings/python/samples/harry-2.py
@@ -14,7 +14,7 @@ def configure_matrix():
     options.rows = 32
     options.cols = 64
     options.chain_length = 2
-    options.gpio_slowdown = 5
+    options.gpio_slowdown = 1
     options.brightness = 75
     options.parallel = 1
     options.hardware_mapping = "adafruit-hat"
