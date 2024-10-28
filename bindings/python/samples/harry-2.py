af5aba6ec94e94296f11309f414de5d7306a053d
Harrison Steg <35347832+hsteg@users.noreply.github.com>
2024-10-27 22:27:09 -0400

    Update harry-2.py

--- bindings/python/samples/harry-2.py
+++ bindings/python/samples/harry-2.py
@@ -18,7 +18,7 @@ def configure_matrix():
     options.brightness = 75
     options.parallel = 1
     options.hardware_mapping = "adafruit-hat"
-    options.multiplexing = 1
+    options.multiplexing = 2
     return RGBMatrix(options=options)
 
 matrix = configure_matrix()
