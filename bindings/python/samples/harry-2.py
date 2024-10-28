568946a42d521a19ca0d3da340f44ff23f6b887b
Harrison Steg <35347832+hsteg@users.noreply.github.com>
2024-10-27 21:57:45 -0400

    revert

--- bindings/python/samples/harry-2.py
+++ bindings/python/samples/harry-2.py
@@ -18,7 +18,6 @@ def configure_matrix():
     options.brightness = 75
     options.parallel = 1
     options.hardware_mapping = "adafruit-hat"
-    options.limit_refresh_rate_hz = True
     return RGBMatrix(options=options)
 
 matrix = configure_matrix()
