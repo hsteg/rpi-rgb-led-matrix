76b41c17fecaf7c8561969c95e568a18ec856a20
Harrison Steg <35347832+hsteg@users.noreply.github.com>
2024-10-27 22:20:23 -0400

    try really low refresh

--- bindings/python/samples/harry-2.py
+++ bindings/python/samples/harry-2.py
@@ -18,6 +18,7 @@ def configure_matrix():
     options.brightness = 75
     options.parallel = 1
     options.hardware_mapping = "adafruit-hat"
+    options.limit_refresh_rate_hz = 5
     return RGBMatrix(options=options)
 
 matrix = configure_matrix()
