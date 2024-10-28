be75adbce131c94fc4b092b1ce4c3700543dbb0d
Harrison Steg <35347832+hsteg@users.noreply.github.com>
2024-10-27 21:44:08 -0400

    try

--- bindings/python/samples/harry-2.py
+++ bindings/python/samples/harry-2.py
@@ -45,8 +45,7 @@ red = graphics.Color(213, 0, 0)
 yellow = graphics.Color(201, 181, 0)
 
 def run():
-    matrix = configure_matrix()
-    print("Current Time:", datetime.now())
+    matrix.brightness = 10
     transit_times = get_transit()
     matrix.Clear()
     if transit_times:
