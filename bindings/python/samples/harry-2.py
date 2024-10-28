db771ef7b95db501c2cc721f811ed03f4e569d59
Harrison Steg <35347832+hsteg@users.noreply.github.com>
2024-10-27 22:30:25 -0400

    Update harry-2.py

--- bindings/python/samples/harry-2.py
+++ bindings/python/samples/harry-2.py
@@ -44,7 +44,7 @@ red = graphics.Color(213, 0, 0)
 yellow = graphics.Color(201, 181, 0)
 
 def run():
-    matrix.brightness = 50
+    matrix.brightness = 75
     transit_times = get_transit()
     matrix.Clear()
     if transit_times:
